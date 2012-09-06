99cfacecefa730f7f8b6317abff267bdaf190b5f
Jerome Andrieux | af83 <jerome.andrieux@af83.com>
2012-09-06 15:46:46 +0200

    Create postit, not linking to board_id yet

--- api/postits.go
+++ api/postits.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
 	"net/http"
+	"time"
+	"fmt"
+	"encoding/json"
+	"strconv"
 )
 
 type Postit struct {
@@ -13,13 +18,15 @@ type Postit struct {
 	//FIXME : corners 
 }
 
-
+var Postits []Postit
 
 func ListPostits(w http.ResponseWriter, req *http.Request) {
 	board_id := req.URL.Query().Get("Board_id")
-	io.WriteString(w, "Listing postits: "+board_id+"\n")
-	// FIXME : without parameters, return nothing
-	// with a valid board_d, return the list
+	if board_id != "" {
+		//FIXME: return JSON array
+		io.WriteString(w, "Listing postits: "+board_id+"\n")
+	}	
+	fmt.Printf("Available postits=%v\n", Postits)
 }
 
 func ShowPostit(w http.ResponseWriter, req *http.Request) {
@@ -28,7 +35,24 @@ func ShowPostit(w http.ResponseWriter, req *http.Request) {
 }
 
 func CreatePostit(w http.ResponseWriter, req *http.Request) {
-	//FIXME : refuse without a valid board id
-	io.WriteString(w, "Creating postit for board #"+req.URL.Query().Get("board_id")+"!\n")
+	postit := &Postit{}	
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	
+	erru := json.Unmarshal(body, &postit)
+	if erru != nil {
+		fmt.Println("Cannot unmarshal to postit: %s", err)
+	}
+	
+	// FIXME : move that somewhere else, with a decent UID
+	postit.Id = strconv.FormatInt(time.Now().UnixNano(), 10)
+	postit.Coords = [2]int{1,1}
+
+	// FIXME : add postit to relevant board postits list
+	Postits = append(Postits, *postit)
+
 }
 
