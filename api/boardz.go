43c13f88b427d8c6dff7b06c96f01a49f899040b
Bruno Michel <bruno.michel@af83.com>
2012-09-06 18:51:00 +0200

    Go fmt

--- api/boardz.go
+++ api/boardz.go
@@ -1,42 +1,42 @@
-package main 
+package main
 
 import (
-  "flag"
-  "fmt"
-  "github.com/bmizerany/pat"
-  "log"
-  "net/http"
+	"flag"
+	"fmt"
+	"github.com/bmizerany/pat"
+	"log"
+	"net/http"
 )
 
 func main() {
-  var addr string
-  var port int
-  var mongoip string 
+	var addr string
+	var port int
+	var mongoip string
 
-  flag.StringVar(&addr, "addr", "127.0.0.1", "Address")
-  flag.IntVar(&port, "port", 8080, "Port") 
-  flag.StringVar(&mongoip, "mongoip", "127.0.0.1", "MongoDB Ip")
-  flag.Parse()
-  
-  m := pat.New()
+	flag.StringVar(&addr, "addr", "127.0.0.1", "Address")
+	flag.IntVar(&port, "port", 8080, "Port")
+	flag.StringVar(&mongoip, "mongoip", "127.0.0.1", "MongoDB Ip")
+	flag.Parse()
 
-  // Boards
-  m.Get("/boards", http.HandlerFunc(ListBoards))
-  m.Post("/boards", http.HandlerFunc(CreateBoard))
-  m.Get("/boards/:Id", http.HandlerFunc(ShowBoard))
-  m.Post("/boards/:Id/postits", http.HandlerFunc(CreatePostit))
-  m.Del("/boards/:Id", http.HandlerFunc(DeleteBoard))
+	m := pat.New()
 
-  // Postits
-  m.Put("/postits/:Id", http.HandlerFunc(UpdatePostit))
+	// Boards
+	m.Get("/boards", http.HandlerFunc(ListBoards))
+	m.Post("/boards", http.HandlerFunc(CreateBoard))
+	m.Get("/boards/:Id", http.HandlerFunc(ShowBoard))
+	m.Post("/boards/:Id/postits", http.HandlerFunc(CreatePostit))
+	m.Del("/boards/:Id", http.HandlerFunc(DeleteBoard))
 
-  http.Handle("/", m)
+	// Postits
+	m.Put("/postits/:Id", http.HandlerFunc(UpdatePostit))
 
-  // Start the HTTP server
-  log.Printf("Listening on http://%s:%d/\n", addr, port)
-  foo := fmt.Sprintf("%s:%d", addr, port)  
-  err := http.ListenAndServe(foo, nil)
-  if err != nil {
-  log.Fatal("ListenAndServe: ", err)
-  }
+	http.Handle("/", m)
+
+	// Start the HTTP server
+	log.Printf("Listening on http://%s:%d/\n", addr, port)
+	foo := fmt.Sprintf("%s:%d", addr, port)
+	err := http.ListenAndServe(foo, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
--- api/postits.go
+++ api/postits.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-  "io/ioutil"
-  "net/http"
-  "time"
-  "fmt"
-  "encoding/json"
-  "strconv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 type Postit struct {
-  Id string
-  Title string
-  Coords [2]int
-  Color string
-  //FIXME : corners 
+	Id     string
+	Title  string
+	Coords [2]int
+	Color  string
+	//FIXME : corners 
 }
 
 func CreatePostit(w http.ResponseWriter, req *http.Request) {
-  board_id := req.URL.Query().Get(":Id")
-  board := boards.findById(board_id)
-  if board == nil {
-    fmt.Println("Board doesn't exist")
-    w.WriteHeader(404)
-    return
-  }
+	board_id := req.URL.Query().Get(":Id")
+	board := boards.findById(board_id)
+	if board == nil {
+		fmt.Println("Board doesn't exist")
+		w.WriteHeader(404)
+		return
+	}
 
-  postit := &Postit{}
-  defer req.Body.Close()
-  body, err := ioutil.ReadAll(req.Body)
-  if err != nil {
-    fmt.Printf("%s", err)
-  }
+	postit := &Postit{}
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
 
-  erru := json.Unmarshal(body, &postit)
-  if erru != nil {
-    fmt.Println("Cannot unmarshal to postit: %s", err)
-  }
+	erru := json.Unmarshal(body, &postit)
+	if erru != nil {
+		fmt.Println("Cannot unmarshal to postit: %s", err)
+	}
 
-  // FIXME : move that somewhere else, with a decent UID
-  postit.Id = strconv.FormatInt(time.Now().UnixNano(), 10)
-  postit.Coords = [2]int{1,1}
-  postit.Color = "#123456"
+	// FIXME : move that somewhere else, with a decent UID
+	postit.Id = strconv.FormatInt(time.Now().UnixNano(), 10)
+	postit.Coords = [2]int{1, 1}
+	postit.Color = "#123456"
 
-  fmt.Printf("%v", *board)
-  fmt.Printf("%v", postit)
+	fmt.Printf("%v", *board)
+	fmt.Printf("%v", postit)
 
-  (*board).addPostit(postit)
+	(*board).addPostit(postit)
 }
 
 func UpdatePostit(w http.ResponseWriter, req *http.Request) {
-  // FIXME : update a postit
+	// FIXME : update a postit
 }
 
 func DeletePostit(w http.ResponseWriter, req *http.Request) {
-  // FIXME : delete a postit
+	// FIXME : delete a postit
 }
