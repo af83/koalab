4aa1b22e05a74c8fe418a878f0c991bce8c3f340
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-09-07 14:24:01 +0200

    Fix connections and express api

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,15 +1,15 @@
 var path     = require('path'),
     express  = require('express'),
-    mongoose = require('mongoose'),
-    hamljs   = require('hamljs');
+    engines = require('consolidate'),
+    mongoose = require('mongoose');
 
-var db = mongoose.createConnection('localhost', null);
+var db = mongoose.createConnection('localhost', 'boardz');
 
-var app = express.createServer();
-app
-   .register('.haml', hamljs)
+var app = express();
+
+app.engine('haml', engines.haml)
    .set('view engine', 'haml')
    .set('view options', {layout: false})
    .set('views', __dirname + '/app/views')
    .set('db', db)
-   .set('models', require('app/models')(db));
+   .set('models', require('./app/models')(db));
