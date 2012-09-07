2760e65c72617efa2e1adb34ecafcf5f6f9ae427
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-09-07 13:54:41 +0200

    Remove Sequelize ORM

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,14 +1,9 @@
-var path      = require('path'),
-    express   = require('express'),
-    Sequelize = require('sequelize'),
-    hamljs    = require('hamljs');
+var path     = require('path'),
+    express  = require('express'),
+    mongoose = require('mongoose'),
+    hamljs   = require('hamljs');
 
-var db  = new Sequelize('database', 'root', null, {
-      dialect: 'sqlite',
-      storage: __dirname + '/db/test.db',
-      sync: { force: true },
-      define : {underscored: true} //column created_at instead of createdAt
-    });
+var db = mongoose.createConnection('localhost', 'password');
 
 var app = express.createServer();
 app
@@ -17,4 +12,4 @@ app
    .set('view options', {layout: false})
    .set('views', __dirname + '/app/views')
    .set('db', db)
-   .set('models', require('app/models')(db, Sequelize));
+   .set('models', require('app/models')(db));
