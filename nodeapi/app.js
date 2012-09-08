b523b830f6c79c4391cc874a1c78994f1f362b96
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-09-08 11:34:28 +0200

    Controller refacto

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -3,18 +3,18 @@ var path     = require('path'),
     engines = require('consolidate'),
     mongoose = require('mongoose');
 
-app = express();
+var app = express();
+app.engine('haml', engines.haml)
+     .set('view engine', 'haml')
+     .set('view options', {layout: false})
+     .set('views', __dirname + '/app/views');
 
 var db = mongoose.createConnection('localhost', 'boardz');
 
 db.once('open', function () {
-  app.engine('haml', engines.haml)
-     .set('view engine', 'haml')
-     .set('view options', {layout: false})
-     .set('views', __dirname + '/app/views')
-     .set('models', require('./app/models')(db));
+  app.set('models', require('./app/models')(db));
 
-  require('./app/controller');
-  app.listen(8080);
-  console.log("http://localhost:8080/");
+  require('./app/controller')(app);
+
+  app.listen(process.env.PORT || 8080);
 });
