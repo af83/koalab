2540fcdb8150faa0394d9acd45a6bd74e578163d
Bruno Michel <bmichel@menfin.info>
2012-09-19 18:08:28 +0200

    Don't hardcode the secret

--- koalab.js
+++ koalab.js
@@ -1,9 +1,22 @@
-var express   = require('express'),
+var fs        = require('fs'),
+    crypto    = require('crypto'),
+    express   = require('express'),
     pass      = require('passport'),
     BrowserID = require('passport-browserid').Strategy,
     mongoose  = require('mongoose');
 
 var db, app = express();
+
+function useSecret(callback) {
+  fs.readFile('.secret', 'utf8', function(err, secret) {
+    if (!err) { return callback(secret); }
+    crypto.randomBytes(48, function(err, buf) {
+      secret = buf.toString('hex');
+      fs.writeFile('.secret', secret, 'utf8');
+      callback(secret);
+    });
+  });
+}
 
 pass.serializeUser(function(user, done) {
   return done(null, user.email);
@@ -23,35 +36,37 @@ pass.use('browserid', new BrowserID({
   }
 ));
 
-app.configure(function() {
-  app.set('port', process.env.PORT || 8080);
-  app.set('view engine', 'jade');
-  app.set('views', __dirname + '/app/views');
+useSecret(function(secret) {
+  app.configure(function() {
+    app.set('port', process.env.PORT || 8080);
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/app/views');
 
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'koalabsecret' }));
-  app.use(pass.initialize());
-  app.use(pass.session());
-  app.use(express.methodOverride());
-  app.use(app.router);
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: secret }));
+    app.use(pass.initialize());
+    app.use(pass.session());
+    app.use(express.methodOverride());
+    app.use(app.router);
 
-  db = mongoose.createConnection('localhost', 'koalab');
-});
+    db = mongoose.createConnection('localhost', 'koalab');
+  });
 
-app.configure('development', function() {
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.logger('dev'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+  app.configure('development', function() {
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
 
-app.configure('production', function() {
-  app.use(express.errorHandler());
-});
+  app.configure('production', function() {
+    app.use(express.errorHandler());
+  });
 
-db.once('open', function () {
-  require('./app/controller')(app, db, pass);
-  app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+  db.once('open', function () {
+    require('./app/controller')(app, db, pass);
+    app.listen(app.get('port'), function() {
+      console.log('Express server listening on port ' + app.get('port'));
+    });
   });
 });
